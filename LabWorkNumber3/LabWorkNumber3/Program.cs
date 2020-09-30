ee68956e8d5357e4e0e6ccf8f7364b34b853da57
Admin <slidercruz999@gmail.com>
2020-09-30 17:49:59 +0600

    Changed code

---
+++ LabWorkNumber3/LabWorkNumber3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LabWorkNumber3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Oleg!");
+
+        }
+    }
+}
