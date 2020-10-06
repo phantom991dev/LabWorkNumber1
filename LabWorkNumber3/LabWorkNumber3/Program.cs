ce176149cdf8a13917f5376fdc2485b6a0a3ef2d
phantom991dev <slidercruz999@gmail.com>
2020-10-06 12:05:49 +0600

    Change lab3

--- LabWorkNumber3/LabWorkNumber3/Program.cs
+++ LabWorkNumber3/LabWorkNumber3/Program.cs
@@ -6,8 +6,18 @@ namespace LabWorkNumber3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Oleg!");
+            double R, x, y, z;
+            Console.Write("Введите R: ");
+            R = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Введите x: ");
+            x = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Введите y: ");
+            y = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Введите z: ");
+            z = Convert.ToSingle(Console.ReadLine());
 
         }
     }
