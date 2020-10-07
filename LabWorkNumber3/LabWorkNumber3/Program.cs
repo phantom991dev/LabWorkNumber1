a95277833e91cdf1e09c1347bb52a144777cbe19
phantom991dev <slidercruz999@gmail.com>
2020-10-07 15:17:33 +0600

    FULL CODE 100%%%

--- LabWorkNumber3/LabWorkNumber3/Program.cs
+++ LabWorkNumber3/LabWorkNumber3/Program.cs
@@ -6,22 +6,6 @@ namespace LabWorkNumber3
     {
         static void Main(string[] args)
         {
-            //начало программы лаб 3
-
-            /*double R, x, y, z;
-            Console.Write("Введите R: ");
-            R = Convert.ToSingle(Console.ReadLine());
-
-            Console.Write("Введите x: ");
-            x = Convert.ToSingle(Console.ReadLine());
-
-            Console.Write("Введите y: ");
-            y = Convert.ToSingle(Console.ReadLine());
-
-            Console.Write("Введите z: ");
-            z = Convert.ToSingle(Console.ReadLine());
-
-            R = Math.Sqrt(2 * (x)) + 2 * y - 4 * x * y + z;*/
             //начало программы
             double r, x, y, z;
             Console.Write("Введите r = ");
